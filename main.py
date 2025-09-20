9c916d6029bb8563273b5803ddbd2d6efbb22991
Irina Batova <BatIr_74_2@mail.ru>
2025-09-20 22:34:00 +0300

    Changes and improvements in main.py and widget.py

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-import os
-current_directory = os.getcwd()
-print(current_directory)
+from src import widget
 
-from src import masks
+# Запрос типа и номера карты или счета и его номера пользователя
+card_or_account_number_user = input("Введите тип карты и её номер или слово 'Счет' и его номер :  ")
 
-card_number_user = input("Введите номер карты:  ")  # запрос номера карты пользователя
-card_number_user = card_number_user.replace(" ", "")  # убираем пробелы, если они есть
+print(widget.mask_account_card(card_or_account_number_user))
 
-while not card_number_user.isdigit() or len(card_number_user) != 16:  # проверяем, что символы в строке цифры и их 16
-    print("Номер карты должен содержать 16 цифр")
-    card_number_user = input("Введите номер карты:  ")  # запрос номера карты пользователя
-    card_number_user = card_number_user.replace(" ", "")  # убираем пробелы, если они есть
+# Запрос пользователя ввести дату
+date_user = input("Введите дату:  ")
 
-card_number_user_int = int(card_number_user)  # меняем тип на целочисленный
-
-print(masks.get_mask_card_number(card_number_user_int))
-
-
-account_number_user = input("Введите номер счета:  ")  # запрос номера счета пользователя
-account_number_user = str(account_number_user).replace(" ", "")  # убираем пробелы, если они есть
-
-while (
-    not account_number_user.isdigit() or len(account_number_user) < 6
-):  # проверяем, что символы в строке цифры и их не < 6
-    print("Номер счета должен содержать минимум 6 цифр")
-    account_number_user = input("Введите номер счета:  ")  # запрос номера карты пользователя
-    account_number_user = str(account_number_user).replace(" ", "")  # убираем пробелы, если они есть
-
-account_number_user_int = int(account_number_user)  # меняем тип на целочисленный
-
-print(masks.get_mask_account(account_number_user_int))
+print(widget.get_date(date_user))
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,5 @@
 from src import masks
 
-card_or_account_number_user = "Maestro 7000792289606361"
-
 def mask_account_card(card_or_account_number:str) -> str:
     """Функция маскировки номера банковской карты
        :param card_or_account_number: принимает тип и номер карты или счета в виде строки
@@ -26,19 +24,14 @@ def mask_account_card(card_or_account_number:str) -> str:
     else:
         number_str = masks.get_mask_card_number(int("".join(number)))
 
-    return print(f"{card_or_account_str}{number_str}")
+    return card_or_account_str + number_str
 
-date_user = "2024-03-11T02:26:18.671407"
 
 def get_date(data:str) -> str:
     """Функция переформатирования даты
-        :param data: принимает дату в виде строки
-        :return: возвращает переформатированную дату - в виде строки
-         формата "ДД.ММ.ГГГГ" ( "11.03.2024" )."""
+        :param data: принимает дату в виде строки - 2024-03-11T02:26:18.671407
+        :return: возвращает переформатированную дату - в виде строки формата "ДД.ММ.ГГГГ" ( "11.03.2024" )."""
 
     formatted_date = '"ДД.ММ.ГГГГ" ( "' + data[8:10] + "." + data[5:7] + "." + data[:4] + '" )'
 
-    return print(formatted_date)
-
-mask_account_card(card_or_account_number_user)
-get_date(date_user)
+    return formatted_date
