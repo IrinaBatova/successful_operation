60b5e435ec78063aaac1d42f62c0be30a7a32188
Irina Batova <BatIr_74_2@mail.ru>
2025-09-26 23:26:49 +0300

    Corrections have been made to the processing.py module, and the main.py module has been updated.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from src import widget
+from src import processing, widget
 
-# Запрос типа и номера карты или счета и его номера пользователя
+# Домашняя работа к уроку 9.2 Основы Git
+
+# Запрос типа и номера карты, или счета и его номера у пользователя
 card_or_account_number_user = input("Введите тип карты и её номер или слово 'Счет' и его номер :  ")
 
 print(widget.mask_account_card(card_or_account_number_user))
@@ -9,3 +11,22 @@ print(widget.mask_account_card(card_or_account_number_user))
 date_user = input("Введите дату:  ")
 
 print(widget.get_date(date_user))
+
+
+# Домашняя работа к уроку 10.1 Продвинутый Git
+
+# Список словарей
+list_of_dictionaries_user = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+# Вызов функции отбора словарей по значению ключа "state"
+print(processing.filter_by_state(list_of_dictionaries_user))
+print(processing.filter_by_state(list_of_dictionaries_user, state="CANCELED"))
+
+# Вызов функции сортировки словарей по значению ключа "date"
+print(processing.sort_by_date(list_of_dictionaries_user))
+print(processing.sort_by_date(list_of_dictionaries_user, ascending=False))
--- src/processing.py
+++ src/processing.py
@@ -1,30 +1,16 @@
-list_of_dictionaries_user = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
 def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> list:
     """Функция отбора словарей по значению ключа "state":
     - param list_of_dictionaries: принимает список словарей;
     - param state: не обязательный, принимает опционально значение для ключа state (по умолчанию 'EXECUTED');
-    - return: возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует выбранному значению.
-    """
+    - return: возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует
+     выбранному значению."""
 
     new_list_of_dictionaries = []
 
     for element in list_of_dictionaries:
         if element.get("state") == state:
             new_list_of_dictionaries.append(element)
-        elif element.get("state") != "EXECUTED":
-            new_list_of_dictionaries.append(element)
-
     return new_list_of_dictionaries
-
-
-print(filter_by_state(list_of_dictionaries_user, state="CANCELED"))
 
 
 def sort_by_date(list_of_dictionaries: list, ascending: bool = True) -> list:
@@ -33,12 +19,6 @@ def sort_by_date(list_of_dictionaries: list, ascending: bool = True) -> list:
     - param ascending: необязательный параметр, задающий порядок сортировки (по умолчанию — убывание);
     - return: возвращает новый список словарей, отсортированный по дате (date)."""
 
-    if not ascending:
-        list_of_dictionaries.sort(key=lambda x: x.get("date", 0))
-    else:
-        list_of_dictionaries.sort(key=lambda x: x.get("date", 0), reverse=True)
+    list_of_dictionaries.sort(key=lambda x: x.get("date", 0), reverse=ascending)
 
     return list_of_dictionaries
-
-
-print(sort_by_date(list_of_dictionaries_user, ascending=False))
