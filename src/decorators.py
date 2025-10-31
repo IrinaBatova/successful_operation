7ab9e829ebf5e416331dee58dfc9680a16590a7e
Irina Batova <BatIr_74_2@mail.ru>
2025-10-31 22:36:11 +0300

    Added log decorator to decorators.py module.

--- src/decorators.py
+++ src/decorators.py
@@ -1,45 +1,66 @@
-import functools
+import functools, datetime
+
 
 # Декоратор log
 def log(filename=None):
+    """
+    Декоратор с параметрами: внешняя функция
+    :param filename: Необязательный аргумент, который определяет куда будут записываться логи (в файл или в консоль):
+        - если filename задан, логи записываются в указанный файл;
+        - если filename не задан, логи выводятся в консоль.
+    :return: Возвращает внутренний декоратор - decorator
+    """
+
     def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        """
+        Декоратор: внутренняя функция
+        :param func: принимает любую функцию, которую нужно декорировать
+        :return: возвращает функцию-обертку: wrapper
+        """
+
+        @functools.wraps(func)  # декоратор передает метаданные (имя функции, docstring)
+        # от декорируемой функции в функцию-обертку.
+        def wrapper(*args, **kwargs):  # функция-обертка
+            log_message_1 = ""
+            log_message_2 = ""
+            log_message_3 = ""
             try:
+                start_time = datetime.datetime.now()
+                log_message_1 = f"{start_time} Starting function '{func.__name__}' with arguments {args} and {kwargs}"
                 result = func(*args, **kwargs)
-                log_message = f"{func.__name__} ok"
+                end_time = datetime.datetime.now()
+                log_message_2 = f"[{end_time}] Finished function '{func.__name__}'"
+                log_message_3 = (
+                    f"{func.__name__} ok. Function execution time: {(end_time - start_time).total_seconds()} seconds"
+                )
                 return result
             except Exception as e:
                 error_type = type(e).__name__
-                log_message = f"{func.__name__} error: {error_type}. Inputs: {args}, {kwargs}"
+                log_message_3 = f"{func.__name__} error: {error_type}. Inputs: {args}, {kwargs}"
                 raise
             finally:
                 if filename:
-                    with open(filename, 'a') as f:
-                        f.write(log_message + '\n')
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(log_message_1 + "\n" + log_message_2 + "\n" + log_message_3 + "\n")
                 else:
-                    print(log_message)
+                    print(log_message_1 + "\n" + log_message_2 + "\n" + log_message_3)
+
         return wrapper
+
     return decorator
 
-# Пример использования
-@log(filename="mylog.txt")
+
+# Пример использования декоратора log, если filename задан, логи выводятся в файл mylog.txt
+@log(filename="../mylog.txt")
 def my_function(x, y):
     return x / y
 
-@log(filename="mylog.txt")
-def my_function(x, y):
+
+# Пример использования декоратора log, если filename не задан, логи выводятся в консоль
+@log()
+def my_function_(x, y):
     return x + y
 
-my_function(1, 2)
 
-# # Пример использования, если filename не задан, логи выводятся в консоль
-# @log()
-# def my_function(x, y):
-#     return x / y
-
-# Вызов функции
-try:
-    my_function(3, 0)
-except ZeroDivisionError:
-    pass
+my_function(10, 5)
+my_function_(10, 5)
