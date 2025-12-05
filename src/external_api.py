21b18ace493da951c50e85c650fd39d02f45f737
Irina Batova <BatIr_74_2@mail.ru>
2025-12-05 21:10:42 +0300

    Changes have been added to the external_api.py module.

--- src/external_api.py
+++ src/external_api.py
@@ -12,24 +12,21 @@ def currency_conversion(amount: str, currency: str) -> float:
     - return: возвращает сумму транзакции в рублях.
     """
 
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+    payload = {}
+    headers = {"apikey": f"{api_key}"}
+
     try:
-        load_dotenv()
-        api_key = os.getenv('API_KEY')
-
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
-        payload = {}
-        headers = {"apikey": f"{api_key}"}
-
         response = requests.request("GET", url=url, headers=headers, data=payload)
-        # status_code = response.status_code
-        # result = response.text # {"message":"You have exceeded your daily\/monthly API rate limit.
-        # Please review and upgrade your subscription plan at https:\/\/promptapi.com\/subscriptions to continue."}
+        result = response.json() # извлекаем данные из ответа в формате JSON и преобразуем их в Python-словарь (dict)
 
-        result = json.loads(response.text)
-        amount_rub = float(result['result'])
-        # print(type(amount_rub))
-
-        return amount_rub
+        if 'result' in result:
+            return float(result['result'])
+        else:
+            print(result)
 
     except requests.exceptions.RequestException as e:
         print(f"HTTP ошибка: {e.response.status_code} - {e.response.reason}")
@@ -45,7 +42,16 @@ def currency_conversion(amount: str, currency: str) -> float:
 
     except Exception as e:
         print(f"Ошибка {e}")
-        print(result)
 
 
-#print(currency_conversion("8221.37", "USD"))
+# print(currency_conversion("8221.37", "USD"))
+
+
+# Результат response.json() - {'success': True,
+#                              'query': {'from': 'USD', 'to': 'RUB', 'amount': 8221.37},
+#                              'info': {'timestamp': 1764936187, 'rate': 76.749016},
+#                              'date': '2025-12-05',
+#                              'result': 630982.057672}
+
+# Если превышен лимит обращений к API, ответ # {"message":"You have exceeded your daily\/monthly API rate limit.
+# Please review and upgrade your subscription plan at https:\/\/promptapi.com\/subscriptions to continue."}
