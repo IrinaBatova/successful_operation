564e376e26dd81de8e6b08ba7615ebdd1450b40e
Irina Batova <BatIr_74_2@mail.ru>
2025-12-05 21:13:44 +0300

    The test_external_api.py module has been created and tests have been added.

---
+++ tests/test_external_api.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from unittest.mock import patch
+from src.external_api import currency_conversion
+
+# Тестирование функции currency_conversion
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+@patch('requests.request')
+def test_currency_conversion_1(mock_request):
+    mock_request.return_value.json.return_value = {'result': 630982.057672} # Настраиваем возвращаемое значение макета
+    result = currency_conversion("8221.37", "USD")
+    assert result == 630982.057672, "Конвертация валюты не прошла как ожидалось" # Проверяем, что функция вернула ожидаемый результат
+
+    # Проверяем, что функция requests.request была вызвана только один раз и с определенными аргументами
+    mock_request.assert_called_once_with(
+        "GET",
+        url="https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=8221.37",
+        headers={"apikey": f"{api_key}"},
+        data={}
+    )
+
+
+def test_currency_conversion_2():
+    with patch('requests.request') as mock_request:
+        mock_request.return_value.json.return_value = {'result': 630982.057672} # Настраиваем возвращаемое значение макета
+        result = currency_conversion("8221.37", "USD")
+        assert result == 630982.057672, "Конвертация валюты не прошла как ожидалось" # Проверяем, что функция вернула ожидаемый результат
+
+        # Проверяем, что функция requests.request была вызвана только один раз и с определенными аргументами
+        mock_request.assert_called_once_with(
+            "GET",
+            url="https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=8221.37",
+            headers={"apikey": f"{api_key}"},
+            data={}
+        )
+
