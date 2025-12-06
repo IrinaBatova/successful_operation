5a8b4bdd6188c4f04391a62ed66956153e0d2840
Irina Batova <BatIr_74_2@mail.ru>
2025-12-06 16:34:44 +0300

    Added fixtures for testing the transaction_amount function to the conftest.py module.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,48 @@
 import pytest
 
+# Тестирование модуля utils.py
+
+# Фикстуры для тестирования функции transaction_amount
+
+@pytest.fixture
+def transactions_utils_rub() -> dict:  # Входные данные
+    transactions_utils_rub = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }
+
+    return transactions_utils_rub
+
+@pytest.fixture
+def transactions_utils_usd() -> dict:  # Входные данные
+    transactions_utils_usd = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+            "amount": "8221.37",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+    }
+    return transactions_utils_usd
+
 # Тестирование модуля generators.py
 
 # Фикстуры для тестирования функции filter_by_currency
