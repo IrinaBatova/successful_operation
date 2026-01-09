9e2200fe34230de98a09c7cd23653a710294e3e1
Irina Batova <BatIr_74_2@mail.ru>
2026-01-09 18:03:43 +0300

    Module processing.py in package src has been modified.

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
-from utils import read_json_file
+from src.utils import read_json_file
+from src import utils, data_import
 from pathlib import Path
 
 
@@ -18,6 +19,10 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> list
     for element in list_of_dictionaries:
         if element.get("state") == state:
             new_list_of_dictionaries.append(element)
+
+    if not new_list_of_dictionaries: # Заданное state не найдено
+        return []
+
     return new_list_of_dictionaries
 
 
@@ -44,9 +49,10 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
     """
 
     new_list_of_dictionaries = []
+    search = search.lower()
 
     for operation in data:
-        if 'description' in operation and re.search(search, operation['description']):
+        if 'description' in operation and re.search(search, str(operation['description']).lower()):
             new_list_of_dictionaries.append(operation)
     return new_list_of_dictionaries
 
@@ -70,22 +76,27 @@ def process_bank_operations(data: list[dict], categories: list) -> dict:
 
 
 if __name__ == "__main__":
-    file_path = str(Path(__file__).parent.parent / "data" / "operations.json")
-    list_of_dictionaries_ = read_json_file(path_to_file=file_path)
+    # file_path = str(Path(__file__).parent.parent / "data" / "operations.json")
+    # list_of_dictionaries_ = utils.read_json_file(path_to_file=file_path)
+    # file_path = str(Path(__file__).parent.parent / "data" / "transactions.csv")
+    # list_of_dictionaries_ = data_import.read_csv_file(path_to_file=file_path)
+    file_path = str(Path(__file__).parent.parent / "data" / "transactions_excel.xlsx")
+    list_of_dictionaries_ = data_import.read_excel_file(path_to_file=file_path)
 
-    # Вызов функции отбора словарей по значению ключа "state"
-    print(filter_by_state(list_of_dictionaries_))
-    print(filter_by_state(list_of_dictionaries_, state="CANCELED"))
 
-    # Вызов функции сортировки словарей по значению ключа "date"
-    print(sort_by_date(list_of_dictionaries_))
-    print(sort_by_date(list_of_dictionaries_, ascending=False))
-
+    # # Вызов функции отбора словарей по значению ключа "state"
+    # print(filter_by_state(list_of_dictionaries_))
+    # print(filter_by_state(list_of_dictionaries_, state="CANCELED"))
+    #
+    # # Вызов функции сортировки словарей по значению ключа "date"
+    # print(sort_by_date(list_of_dictionaries_))
+    # print(sort_by_date(list_of_dictionaries_, ascending=False))
+    #
     # Вызов функции отбора словарей где в значении ключа "description" есть, заданная строка
-    search_bar = "Перевод со счета на счет"
+    search_bar = "открытие"
     print(process_bank_search(list_of_dictionaries_, search_bar))
 
-    # Вызов функции возвращающей словарь, где ключи: названия категорий, значения: количество операций каждой категории
-    categories_list = ["Перевод со счета на счет", "Открытие вклада", "Перевод организации",
-                       "Перевод с карты на карту", "Перевод с карты на счет"]
-    print(process_bank_operations(list_of_dictionaries_, categories_list))
+    # # Вызов функции возвращающей словарь, где ключи: названия категорий, значения: количество операций каждой категории
+    # categories_list = ["Перевод со счета на счет", "Открытие вклада", "Перевод организации",
+    #                    "Перевод с карты на карту", "Перевод с карты на счет"]
+    # print(process_bank_operations(list_of_dictionaries_, categories_list))
